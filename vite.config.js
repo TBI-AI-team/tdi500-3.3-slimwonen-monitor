b365afe63e9ed34b3f51c28e9f1aaf72098a9253
Jeroen Pat <jeroen.pat@tbi.nl>
2026-05-28 11:56:00 +0200

    fix: base path to match repo name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tdi500-slimwonen-monitor/',
+  base: '/tdi500-3.3-slimwonen-monitor/',
   server: { port: 3000 },
 });
